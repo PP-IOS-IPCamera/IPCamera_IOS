ce1704119203eeece2e6b99d7db39cba72c6abe7
hiemseyha <hiemseyha168@gmail.com>
2016-12-27 10:41:41 +0700

    First Commit

---
+++ IPCamera_IOS/Controllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IPCamera_IOS
+//
+//  Created by Hiem Seyha on 12/27/16.
+//  Copyright © 2016 seyha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
